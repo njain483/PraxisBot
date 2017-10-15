218610f7c9f71d2601cdf568f6173d32fa9bc55b
njain483 <njain483@gmail.com>
2017-10-15 11:45:40 +0530

    Final Update

--- praxisbot.js
+++ praxisbot.js
@@ -1,17 +1,35 @@
-
-/** This is a sample code for your bot**/
+var intentname="";
 function MessageHandler(context, event) {
     // var nlpToken = "xxxxxxxxxxxxxxxxxxxxxxx";//Your API.ai token
     // context.sendResponse(JSON.stringify(event));
     sendMessageToApiAi({
         message : event.message,
         sessionId : new Date().getTime() +'api',
-        nlpToken : "4ce699a0f6a04c4392feb70f83167ebf",
+        nlpToken : "227ade3cb57f476dabc1a3d6644c5613",
         callback : function(res){
-            //Sample response from apiai here.
+          var loc="";
+          intentname=JSON.parse(res).result.metadata.intentName;
+          if (intentname=="weather" && JSON.parse(res).result.parameters.geocity!="")
+            callWeatherAPI(JSON.parse(res).result.parameters.geocity);
+          else if (intentname=="pagespeed" && JSON.parse(res).result.parameters.url!="")
+            callPageSpeedAPI(JSON.parse(res).result.parameters.url);
+          else
             context.sendResponse(JSON.parse(res).result.fulfillment.speech);
         }
     },context)
+}
+
+function callPageSpeedAPI(url)
+{
+
+  var apiurl="https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=http%3A%2F%2F"+url;
+  context.simplehttp.makeGet(apiurl);
+}
+
+function callWeatherAPI(loc)
+{
+  var url="https://api.apixu.com/v1/current.json?key=9e854b895368407c979120711171410&q="+loc;
+  context.simplehttp.makeGet(url);
 }
 
 function sendMessageToApiAi(options,botcontext) {
@@ -48,12 +66,28 @@ function EventHandler(context, event) {
         context.simpledb.botleveldata.numinstance = 0;
     numinstances = parseInt(context.simpledb.botleveldata.numinstance) + 1;
     context.simpledb.botleveldata.numinstance = numinstances;
-    context.sendResponse("Thanks for adding me. You are:" + numinstances);
+    context.sendResponse("Hello "+event.senderobj.display+"\r\nI am Praxis & how can I help you?\r\n\r\nTo know more just reply with 'more info' or 'about praxis'\r\n\r\nOr simply visit this link:\r\nbit.ly/praxisbot");
 }
 
 function HttpResponseHandler(context, event) {
     // if(event.geturl === "http://ip-api.com/json")
-    context.sendResponse(event.getresp);
+    if (intentname=="weather") {
+      var weatherapi=JSON.parse(event.getresp);
+      var currentweather=weatherapi.current.condition.text;
+      currentweather = "Today's Weather: "+currentweather+"\r\nTemperature: "+weatherapi.current.temp_c+" °C";
+      var finalweather=
+      [currentweather,
+        {"type":"image","originalUrl":"http:"+weatherapi.current.condition.icon,"previewUrl":"http:"+weatherapi.current.condition.icon},
+        {"type":"link","href":"https://whatsapp.com/favicon.png"}
+      ];
+      context.sendResponse(JSON.stringify(finalweather));
+    }
+    else if (intentname=="pagespeed") {
+      var pagespeedapi=JSON.parse(event.getresp);
+      var score=pagespeedapi.score;
+      score = "Score for "+pagespeedapi.title+" is "+score+"/100";
+      context.sendResponse(score);
+    }
 }
 
 function DbGetHandler(context, event) {
